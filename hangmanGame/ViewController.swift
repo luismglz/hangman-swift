75f97c7b1a5fa03e13f231042b97747fcc6d618e
Luis GL <lgl66505@udelasalle.edu.mx>
2022-05-13 23:20:56 -0500

    2 players available

--- hangmanGame/ViewController.swift
+++ hangmanGame/ViewController.swift
@@ -4,14 +4,25 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var totalLifes = 3{
+    var totalLifesPlayer1 = 3{
         didSet {
-            imgLifeSpan.image = UIImage(named: "lifes\(totalLifes)")
+            imgLifeSpanPlayer1.image = UIImage(named: "lifes\(totalLifesPlayer1)")
         }
     }
     
-    var score = 0
-    var perTurnLifes = 6
+    var totalLifesPlayer2 = 3{
+        didSet {
+            imgLifeSpanPlayer2.image = UIImage(named: "lifes\(totalLifesPlayer2)")
+        }
+    }
+    
+    
+    
+    var scorePlayer1 = 0
+    var perTurnLifesPLayer1 = 6
+    
+    var scorePlayer2 = 0
+    var perTurnLifesPLayer2 = 6
     
     var isCompleteGame = false
     var chosen5CharWord = "";
@@ -23,7 +34,16 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var btnStartGame: UIButton!
     
-    @IBOutlet weak var imgLifeSpan: UIImageView!
+    
+    
+    
+    @IBOutlet weak var imgLifeSpanPlayer1: UIImageView!
+    @IBOutlet weak var imgLifeSpanPlayer2: UIImageView!
+    
+    
+    @IBOutlet weak var lblPlayer1: UILabel!
+    @IBOutlet weak var lblPlayer2: UILabel!
+    
     
     @IBOutlet weak var labelWord: UILabel!
     
@@ -41,6 +61,11 @@ class ViewController: UIViewController {
     var usedLetters = ""
 
     var level = 0
+    
+    var currentPlayerFlag = 0
+    
+    var isPlayer2Flag = false
+    
     
     @IBAction func onGenerate(_ sender: Any) {
         enableButtons()
@@ -69,6 +94,9 @@ class ViewController: UIViewController {
         
     
     func generateRandomWord(){
+        validateUser()
+        currentPlayerFlag += 1
+        
         btnStartGame.isHidden = true;
         let randomList = "wordList\(arc4random_uniform(3)+1)"
         let randomWordIndex = Int((arc4random_uniform(5)));
@@ -93,7 +121,22 @@ class ViewController: UIViewController {
         
     }
     
+    func isPlayer2()-> Bool{
+        currentPlayerFlag % 2 == 0 ? false : true
+    }
     
+    func validateUser(){
+        if(isPlayer2()){
+            isPlayer2Flag = true
+            lblPlayer1.font = UIFont.systemFont(ofSize: 24, weight: .regular)
+            lblPlayer2.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+            
+        }else{
+            isPlayer2Flag = false
+            lblPlayer2.font = UIFont.systemFont(ofSize: 24, weight: .regular)
+            lblPlayer1.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        }
+    }
     
         
     func disableButtons(){
@@ -116,7 +159,7 @@ class ViewController: UIViewController {
         hiddenWord = ""
         hiddenWordInArray = [String]()
         
-        perTurnLifes = 6
+        perTurnLifesPLayer1 = 6
         hangmanImgNumber = 0
         
         //  Save word into an array + string
@@ -163,7 +206,7 @@ class ViewController: UIViewController {
             
         } else {
             hangmanImgNumber += 1
-            perTurnLifes -= 1
+            perTurnLifesPLayer1 -= 1
             
         }
         
@@ -182,50 +225,89 @@ class ViewController: UIViewController {
     
         
     func checkIfTurnIsCompleted(generatedWord: String){
-        print(perTurnLifes)
-        if perTurnLifes > 0 {
+        print(perTurnLifesPLayer1)
+        if perTurnLifesPLayer1 > 0 {
             if hiddenWord == generatedWord {
-                score += 1
-                //score == 3 || totalLifes == score
-                if(score == 3 && totalLifes > 0){
-                    checkIfGameIsCompleted()
+                if isPlayer2Flag{
+                    scorePlayer2 += 1
+                    //score == 3 || totalLifes == score
+                    if(scorePlayer2 == 3 && totalLifesPlayer2 > 0){
+                        checkIfGameIsCompleted()
+                    }else{
+                        displayAlertMessage(title: "Player 2 win the round🎉", alertDescription: "Hangman is happy", alertStyle: alertType.Winner.rawValue)
+                        generateRandomWord()
+                    }
                 }else{
-                    displayAlertMessage(title: "You win the round🎉", alertDescription: "Hangman is happy", alertStyle: alertType.Winner.rawValue)
-                    generateRandomWord()
+                    scorePlayer1 += 1
+                    //score == 3 || totalLifes == score
+                    if(scorePlayer1 == 3 && totalLifesPlayer1 > 0){
+                        checkIfGameIsCompleted()
+                    }else{
+                        displayAlertMessage(title: "Player 1 win the round🎉", alertDescription: "Hangman is happy", alertStyle: alertType.Winner.rawValue)
+                        generateRandomWord()
+                    }
                 }
+                
+                
             }
             
         } else {
             
-            
-            //score -= 1
-            totalLifes -= 1
-            if(totalLifes == 0){
-                checkIfGameIsCompleted()
+            if isPlayer2Flag{
+                //score -= 1
+                totalLifesPlayer2 -= 1
+                if(totalLifesPlayer2 == 0){
+                    checkIfGameIsCompleted()
+                }else{
+                    displayAlertMessage(title: "Player 2 loose the round💀", alertDescription: "You hanged the character... the answer was \"\(generatedWord.uppercased())\"!", alertStyle: alertType.Looser.rawValue)
+                    generateRandomWord()
+                }
             }else{
-                displayAlertMessage(title: "You loose the round💀", alertDescription: "You hanged the character... the answer was \"\(generatedWord.uppercased())\"!", alertStyle: alertType.Looser.rawValue)
-                generateRandomWord()
-            }
+                
+                //score -= 1
+                totalLifesPlayer1 -= 1
+                if(totalLifesPlayer1 == 0){
+                    checkIfGameIsCompleted()
+                }else{
+                    displayAlertMessage(title: "Player 1 loose the round💀", alertDescription: "You hanged the character... the answer was \"\(generatedWord.uppercased())\"!", alertStyle: alertType.Looser.rawValue)
+                    generateRandomWord()
+                }
+                    
+                }
+        }
             
         }
-        
-    }
     
     
     
     
     func checkIfGameIsCompleted(){
-        print("totalLifes", totalLifes)
-        print("score", score)
+        print("totalLifes1", totalLifesPlayer1)
+        print("score1", scorePlayer1)
         
-        if totalLifes > 0 && score==3{
-            isCompleteGame = true
-            displayAlertMessage(title: "You Win 🚀", alertDescription: "Hangman is happy", alertStyle: alertType.Winner.rawValue)
-            restartGame()
-        }else if totalLifes == 0 && score == 0{
-            displayAlertMessage(title: "Game Over 💤", alertDescription: "Hangman is sad", alertStyle: alertType.Looser.rawValue)
-            restartGame()
+        
+        print("totalLifes2", totalLifesPlayer2)
+        print("score2", scorePlayer2)
+        if isPlayer2Flag{
+            if totalLifesPlayer2 > 0 && scorePlayer2==3{
+                isCompleteGame = true
+                displayAlertMessage(title: "Player 2 is winner 🚀", alertDescription: "Hangman is happy", alertStyle: alertType.Winner.rawValue)
+                restartGame()
+            }else if totalLifesPlayer2 == 0 && scorePlayer2 == 0{
+                displayAlertMessage(title: "Player 2 loose game 💤", alertDescription: "Player 1 is winner", alertStyle: alertType.Looser.rawValue)
+                restartGame()
+            }
+        }else{
+            if totalLifesPlayer1 > 0 && scorePlayer1==3{
+                isCompleteGame = true
+                displayAlertMessage(title: "Player 1 is winner 🚀", alertDescription: "Hangman is happy", alertStyle: alertType.Winner.rawValue)
+                restartGame()
+            }else if totalLifesPlayer1 == 0 && scorePlayer1 == 0{
+                displayAlertMessage(title: "Player 1 loose game 💤", alertDescription: "Player 2 is winner", alertStyle: alertType.Looser.rawValue)
+                restartGame()
+            }
         }
+        
         
     }
     
@@ -261,12 +343,26 @@ class ViewController: UIViewController {
     
     func restartGame(){
         disableButtons()
+        
+        currentPlayerFlag = 0
+        isPlayer2Flag = false
+        
         labelWord.text = ""
-        totalLifes = 3
-        score = 0
-        perTurnLifes = 6
+        totalLifesPlayer1 = 3
+        scorePlayer1 = 0
+        perTurnLifesPLayer1 = 6
+        
+        totalLifesPlayer2 = 3
+        scorePlayer2 = 0
+        perTurnLifesPLayer2 = 6
         btnStartGame.isHidden = false
+        
+        lblPlayer1.font = UIFont.systemFont(ofSize: 24, weight: .regular)
+        lblPlayer2.font = UIFont.systemFont(ofSize: 24, weight: .regular)
+        
     }
+
+
 
 
 }
